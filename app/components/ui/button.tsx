1140463719a02bbe118761ebf71891918cb8e915
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-10-01 14:59:07 +0900

    style/shadcn 스타일 수정

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -22,7 +22,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+        sm: "rounded-sm gap-1.5 px-2 has-[>svg]:px-2.5 text-xxs",
         md: "rounded-md px-4 py-4 text-base",
         lg: "rounded-md px-6 has-[>svg]:px-4 py-6 text-base",
         icon: "size-9",
@@ -32,7 +32,7 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  },
+  }
 );
 
 function Button({
